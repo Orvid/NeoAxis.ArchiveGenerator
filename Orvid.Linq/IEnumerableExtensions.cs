5ad4e1ec4f04ca1c793172ce247b19dfd1a3605c
Orvid King <blah38621@gmail.com>
2013-01-07 16:47:03 -0600

    Some modifications

--- Orvid.Linq/IEnumerableExtensions.cs
+++ Orvid.Linq/IEnumerableExtensions.cs
@@ -55,11 +55,69 @@ namespace Orvid.Linq
 			}
 		}
 
+		public static T Min<T>(this IEnumerable<T> pThis) where T : IComparable<T>
+		{
+			using (var e = pThis.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					throw new Exception();
+				T t = e.Current;
+				while (e.MoveNext())
+				{
+					if (t.CompareTo(e.Current) > 0)
+						t = e.Current;
+				}
+				return t;
+			}
+		}
+
 		public static IEnumerable<TResult> Select<T, TResult>(this IEnumerable<T> pThis, Func<T, TResult> selector)
 		{
 			List<TResult> ret = new List<TResult>();
 			pThis.ForEach(t => ret.Add(selector(t)));
 			return ret;
+		}
+
+		public static IEnumerable<T> Sort<T>(this IEnumerable<T> pThis) where T : IComparable<T>
+		{
+			List<T> ret = new List<T>();
+			using (var e = pThis.GetEnumerator())
+			{
+				while (e.MoveNext())
+				{
+					T cur = e.Current;
+					if (ret.Count == 0)
+						ret.Add(cur);
+					else
+					{
+						int i = 0;
+						for (; i < ret.Count; i++)
+						{
+							int r = ret[i].CompareTo(cur);
+							if (r > 0)
+							{
+								ret.Insert(i, cur);
+								break;
+							}
+						}
+						if (i == ret.Count)
+							ret.Add(cur);
+					}
+				}
+			}
+			return ret;
+		}
+
+		public static int Sum(this IEnumerable<int> pThis)
+		{
+			int ret = 0;
+			pThis.ForEach(i => ret += i);
+			return ret;
+		}
+
+		public static int Sum<T>(this IEnumerable<T> pThis, Func<T, int> selector)
+		{
+			return pThis.Select(selector).Sum();
 		}
 
 		public static T[] ToArray<T>(this IEnumerable<T> pThis)
